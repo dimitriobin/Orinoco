361a8dae26673eb7baedde852a6f703b7a041dae
Dimitri Obin <dimitriobin@gmail.com>
2020-08-20 14:45:04 +0200

    Intégration des listes de produits dynamiques

--- front-end/assets/js/index.js
+++ front-end/assets/js/index.js
@@ -1,17 +1,47 @@
-// let xhr = new XMLHttpRequest();
-// xhr.open('GET', 'http://localhost:3000/api/teddies');
-// xhr.responseType = 'json';
-// xhr.onload = function () {
-//     if (xhr.status === 200) {
-//         console.log(xhr.response);
-//     } else {
-//         console.error('ERROR');
-//     }
-// };
-// xhr.send();
+let teddiesBtn = document.getElementById('oriTeddies');
+let camerasBtn = document.getElementById('oriCameras');
+let furnituresBtn = document.getElementById('oriFurnitures');
 
-// Fetch datas for teddies
-function fetchDatas(url) {
+const teddiesAPI = 'http://localhost:3000/api/teddies';
+const camerasAPI = 'http://localhost:3000/api/cameras';
+const furnitureAPI = 'http://localhost:3000/api/furniture';
+
+
+// How to throw the list in the DOM 
+function showProducts(datas) {
+    let listOfProducts = document.querySelector('#listOfProdutcs');
+    listOfProducts.innerHTML = '';
+    for (let i = 0; i < datas.length; i++) {
+        let article = document.createElement('article');
+        let productImg = datas[i].imageUrl;
+        let productName = datas[i].name;
+        let productDescription = datas[i].description;
+        let productPrice = datas[i].price;
+        let productId = datas[i]._id
+
+        article.setAttribute('class', 'mb-3 border w-100');
+        article.innerHTML = '<div class="row"><div class="col-12 col-md-5 my-auto"><img src=' + productImg + ' alt="Une photographie de ' + productName + '" class="card-img h-100"></div><div class="col-12 col-md-7"><div class="card-body"><h3 class="card-title">' + productName + '</h5><p class="card-text mb-1">' + productDescription + '</p><p class="card-text text-muted mb-1">Prix : ' + productPrice + '</p><a href="pages-html/produits.html?id=' + productId + '" class="stretched-link" aria-label="Voir la fiche de ce produit"></div></div></div>';
+        listOfProducts.appendChild(article);
+    }
+};
+
+// XHR method
+// function getDatas(url) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', url);
+//     xhr.responseType = 'json';
+//     xhr.onload = function () {
+//         if (xhr.status === 200) {
+//             showProducts(xhr.response);
+//         } else {
+//             console.error('ERROR');
+//         }
+//     };
+//     xhr.send();
+// }
+
+// Fetch method
+function getDatas(url) {
     fetch(url)
         .then(function (response) {
             if (response.ok) {
@@ -22,13 +52,16 @@ function fetchDatas(url) {
         }).then(function (data) {
             showProducts(data);
         })
-}
+};
 
-function showProducts(datas) {
-    let
-    for (let i = 0; i < datas.length; i++) {
 
-    }
-}
-
-fetchDatas('http://localhost:3000/api/teddies');
+// Event on theme's buttons 
+teddiesBtn.addEventListener('click', function () {
+    getDatas(teddiesAPI)
+});
+camerasBtn.addEventListener('click', function () {
+    getDatas(camerasAPI)
+});
+furnituresBtn.addEventListener('click', function () {
+    getDatas(furnitureAPI)
+});
